1ad9de75acc807fbae445d13185a3d72567ae939
Reyes Develop <juanda3194236082@gmail.com>
2024-08-16 18:06:42 -0500

    Correcciones Versión final 2
    Se realizo un ajuste para que el proyecto pueda subirse a Githuib pages y arregle un problema en la lógica de js

--- JS/app.js
+++ JS/app.js
@@ -1,40 +1,22 @@
 document.getElementById('btn-encriptar').addEventListener('click', function () {
     const inputTexto = document.getElementById('input-texto').value;
-    const textoEncriptado = encriptarTexto(inputTexto);
-
-    if (textoEncriptado) {
-        // Mostrar el texto encriptado y habilitar el span y el botón de copiar
-        const spanEncriptado = document.getElementById('palabra-encryptada');
-        spanEncriptado.innerText = textoEncriptado;
-        spanEncriptado.style.display = 'inline';
-
-        const btnCopiar = document.getElementById('btn-copiar');
-        btnCopiar.style.display = 'block';
-
-        // Ocultar la imagen, h3, y p
-        document.querySelector('.img-output').style.display = 'none';
-        document.querySelector('.title-output').style.display = 'none';
-        document.querySelector('.parrafo-output').style.display = 'none';
+    
+    if (validarTexto(inputTexto)) {
+        const textoEncriptado = encriptarTexto(inputTexto);
+        mostrarTextoEncriptado(textoEncriptado);
+    } else {
+        alert('El texto contiene caracteres no permitidos. Solo se permiten letras minúsculas sin acentos.');
     }
 });
 
 document.getElementById('btn-desencriptar').addEventListener('click', function () {
     const inputTexto = document.getElementById('input-texto').value;
-    const textoDesencriptado = desencriptarTexto(inputTexto);
-
-    if (textoDesencriptado) {
-        // Mostrar el texto desencriptado y habilitar el span y el botón de copiar
-        const spanEncriptado = document.getElementById('palabra-encryptada');
-        spanEncriptado.innerText = textoDesencriptado;
-        spanEncriptado.style.display = 'inline';
-
-        const btnCopiar = document.getElementById('btn-copiar');
-        btnCopiar.style.display = 'block';
-
-        // Ocultar la imagen, h3, y p
-        document.querySelector('.img-output').style.display = 'none';
-        document.querySelector('.title-output').style.display = 'none';
-        document.querySelector('.parrafo-output').style.display = 'none';
+    
+    if (validarTexto(inputTexto)) {
+        const textoDesencriptado = desencriptarTexto(inputTexto);
+        mostrarTextoEncriptado(textoDesencriptado);
+    } else {
+        alert('El texto contiene caracteres no permitidos. Solo se permiten letras minúsculas sin acentos.');
     }
 });
 
@@ -62,10 +44,7 @@ document.getElementById('btn-limpiar').addEventListener('click', function () {
 });
 
 function encriptarTexto(texto) {
-    texto = texto.toLowerCase()
-        .replace(/[0-9]/g, '')
-        .replace(/[!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~]/g, '')
-        .replace(/[áéíóúÁÉÍÓÚ]/g, '')
+    texto = texto
         .replace(/e/g, 'enter')
         .replace(/i/g, 'imes')
         .replace(/a/g, 'ai')
@@ -76,7 +55,7 @@ function encriptarTexto(texto) {
 }
 
 function desencriptarTexto(texto) {
-    texto = texto.toLowerCase()
+    texto = texto
         .replace(/enter/g, 'e')
         .replace(/imes/g, 'i')
         .replace(/ai/g, 'a')
@@ -84,4 +63,25 @@ function desencriptarTexto(texto) {
         .replace(/ufat/g, 'u');
 
     return texto;
+}
+
+function validarTexto(texto) {
+    // Expresión regular para permitir solo letras minúsculas sin acentos
+    const regex = /^[a-z\s]*$/;
+    return regex.test(texto);
+}
+
+function mostrarTextoEncriptado(texto) {
+    // Mostrar el texto encriptado y habilitar el span y el botón de copiar
+    const spanEncriptado = document.getElementById('palabra-encryptada');
+    spanEncriptado.innerText = texto;
+    spanEncriptado.style.display = 'inline';
+
+    const btnCopiar = document.getElementById('btn-copiar');
+    btnCopiar.style.display = 'block';
+
+    // Ocultar la imagen, h3, y p
+    document.querySelector('.img-output').style.display = 'none';
+    document.querySelector('.title-output').style.display = 'none';
+    document.querySelector('.parrafo-output').style.display = 'none';
 }
